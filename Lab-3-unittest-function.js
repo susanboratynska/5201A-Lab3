e24507728186d2604843dab7498171b1f41286cd
susanboratynska <susan.boratynska@gmail.com>
2020-01-23 10:45:30 -0500

    Initial Commit

---
+++ Lab-3-unittest-function.js
@@ -0,0 +1,8 @@
+//LAB 3 - UNIT TESTING
+//======== THIS FILE IS FOR THE checkHumbrId FUNCTION ==========
+alert("lab 3 Function");//please delete this line once connected.
+/** checkHumbrId Function
+ * Validate input as a Humber College Student number.
+ * Returns true if input validates.
+ * @param {string} idIn
+ */
