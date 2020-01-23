4bdba76612e55a4edc9727efb85e728198793351
Yusha16 <chris.yushi@gmail.com>
2020-01-23 11:41:47 -0500

    Add test function

--- Lab-3-unittest-test.js
+++ Lab-3-unittest-test.js
@@ -1,4 +1,16 @@
 //LAB 3 - UNIT TESTING
 //======== THIS FILE IS FOR THE UNIT TEST ==========
-alert("lab 3 UNIT TEST");//please delete this line once connected.
-
+//alert("lab 3 UNIT TEST");//please delete this line once connected.
+function test__checkHumberId(valueIn, expected) {
+    "use strict";
+    //Call the test function
+    let result = checkHumberId(valueIn);
+	//Create the display message
+    let msg = "Value tested: " + valueIn + " Expected result: xxFAILEDxx";
+    if (expected) {
+        msg = "Value tested: " + valueIn + " Expected result: ==PASSED==";
+    }
+	//Display the message
+    let msgBox = document.getElementById("msgs");
+    msgBox.innerHTML += msg;
+}
